d8b175bf62f895b3e2ba24155ef2260cfb0241ae
Reinhold Melcher <reinhold.melcher@freenet.de>
2023-01-10 22:10:34 +0100

    update

---
+++ publication/export_bibtex.sh
@@ -0,0 +1,7 @@
+cd "E:\Documents\Installationsordner\R\personal_website\content\publication"
+
+E:/Documents/Installationsordner/Anaconda/Scripts/activate
+
+academic import --bibtex _data/publications.bib
+
+
